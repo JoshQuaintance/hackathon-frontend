16e0cd32f3946e82474b2af19d8a44e79299a81d
Joshua Quaintance <45566099+JoshQuaintance@users.noreply.github.com>
2023-02-03 16:11:56 -0500

    Updates

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       host: env('DATABASE_HOST'),
       port: env.int('DATABASE_PORT'),
       database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
+      user: env('DATABASE_USER'),
       password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', false),
     },
